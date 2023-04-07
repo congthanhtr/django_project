9de963d1678947f687b5d623c51a9e9bc575e76e
WIN-F3NN8N83CAK\thanh <congthanhtr151@gmail.com>
2023-04-07 13:01:17 +0700

    login api jwt

--- authen/practiceauth/serializers.py
+++ authen/practiceauth/serializers.py
@@ -1,17 +1,13 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate
 
 class LoginSerializer(serializers.Serializer):
 
     username = serializers.CharField()
     password = serializers.CharField()
 
-    def validate(self, attrs):
-        username = attrs.get('username')
-        password = attrs.get('password')
 
-        if authenticate(username, password):
-            return attrs
-        else:
-            return {'msg': 'invalid credentials'}
+class RegisterSerializer(serializers.Serializer):
+
+    username = serializers.CharField()
+    password = serializers.CharField()
--- authen/practiceauth/views.py
+++ authen/practiceauth/views.py
@@ -5,6 +5,8 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import LoginSerializer
+from django.contrib.auth import authenticate
+from rest_framework_simplejwt.tokens import RefreshToken
 # Create your views here.
 
 class LoginView(APIView):
@@ -15,5 +17,15 @@ class LoginView(APIView):
         if not serializer.is_valid():
             return Response(data = 'Bad Request', status=status.HTTP_400_BAD_REQUEST)
         
-        
-        return Response(data='Unauthorized', status=status.HTTP_401_UNAUTHORIZED)
+        username = request.data.get('username')
+        password = request.data.get('password')
+        user = authenticate(username=username, password=password)
+        if user is None:
+            return Response('Invalid Credentials', status=status.HTTP_401_UNAUTHORIZED)
+    
+        token = RefreshToken().for_user(user=user)
+        return Response(data={
+            'refresh_token': str(token),
+            'access_token': str(token.access_token)
+        }, status=status.HTTP_200_OK)
+
